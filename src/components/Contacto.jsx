b4d8f795f6031b885a11a78646d14b5aa7e337bf
sebacruz1 <sebastiancruzpomar@gmail.com>
2025-07-23 00:48:08 -0400

    error de tipeo

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -22,8 +22,8 @@ export const Contacto = () => {
     <section id="contact" className="py-24 px-4 relative bg-secondary/30">
       <div className="container mx-auto max-w-5xl">
         <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
-          Get In <span className="text-primary"> Touch</span>
-        </h2>
+       		Contáctame!
+ 	</h2>
 
         <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
           ¿Quieres contactarme para algún trabajo o proyecto?
