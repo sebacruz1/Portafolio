3611145fe645e83e5372c916e5a734ce349ae142
sebacruz1 <sebastiancruzpomar@gmail.com>
2025-09-02 21:52:02 -0400

    arreglo de playwright.yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,48 +1,43 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
-const BASEURL = 'http://localhost:4173';
+const BASEURL = "http://localhost:4173";
 
 export default defineConfig({
-  testDir: 'tests',
-  reporter: [
-    ['html', { outputFolder: 'test-results/' }],
-    ['dot'],
-  ],
+  testDir: "tests",
+  reporter: [["html", { outputFolder: "test-results/" }], ["dot"]],
 
   use: {
     baseURL: BASEURL,
-    trace: 'on-first-retry',
-    screenshot: 'only-on-failure',
+    trace: "on-first-retry",
+    screenshot: "only-on-failure",
   },
 
   projects: [
     {
-      name: 'Chromium',
-      use: { ...devices['Desktop Chrome'] },
-      testMatch: ['**/*.desktop.spec.@(js|ts)', '**/*.all.spec.@(js|ts)'],
+      name: "Chromium",
+      use: { ...devices["Desktop Chrome"] },
+      testMatch: ["**/*.desktop.spec.@(js|ts)", "**/*.all.spec.@(js|ts)"],
     },
     {
-      name: 'Desktop Safari',
-      use: { ...devices['Desktop Safari'] },
-      testMatch: ['**/*.desktop.spec.@(js|ts)', '**/*.all.spec.@(js|ts)'],
+      name: "Desktop Safari",
+      use: { ...devices["Desktop Safari"] },
+      testMatch: ["**/*.desktop.spec.@(js|ts)", "**/*.all.spec.@(js|ts)"],
     },
     {
-      name: 'iPhone 15',
-      use: { ...devices['iPhone 15'] },
-      testMatch: ['**/*.mobile.spec.@(js|ts)', '**/*.all.spec.@(js|ts)'],
+      name: "iPhone 15",
+      use: { ...devices["iPhone 15"] },
+      testMatch: ["**/*.mobile.spec.@(js|ts)", "**/*.all.spec.@(js|ts)"],
     },
     {
-      name: 'Pixel 7',
-      use: { ...devices['Pixel 7'] },
-      testMatch: ['**/*.mobile.spec.@(js|ts)', '**/*.all.spec.@(js|ts)'],
-    }
+      name: "Pixel 7",
+      use: { ...devices["Pixel 7"] },
+      testMatch: ["**/*.mobile.spec.@(js|ts)", "**/*.all.spec.@(js|ts)"],
+    },
   ],
 
-  webServer: 
-      {
-        command: 'npm run preview -- --port 4173 --strictPort ', 
-        port: 4173, 
-        reuseExistingServer: true,
-      },
+  webServer: {
+    command: "npm run preview -- --port 4173 --strictPort ",
+    port: 4173,
+    reuseExistingServer: true,
+  },
 });
-
