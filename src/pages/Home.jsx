c0f077ff5c6e74fadfb2066686d2d1c6bb8fee34
sebacruz1 <sebastiancruzpomar@gmail.com>
2025-09-09 12:36:28 -0300

    arreglo importanciones

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,12 @@
-import { Navbar } from "../components/Navbar";
-import { HeroSection } from "../components/HeroSection";
-import { About } from "../components/About";
-import { Skills } from "../components/Skills";
-import { Contacto } from "../components/Contacto"
+import { Navbar } from "@/components/Navbar";
+import { HeroSection } from "@/components/HeroSection";
+import { About } from "@/components/About";
+import { Skills } from "@/components/Skills";
+import { Contacto } from "@/components/Contacto";
 
-export const Home = () => { 
-    return ( 
-        <div className="min-h-screen bg-background text-foreground overflow-x-hidden"> 
-
+export const Home = () => {
+    return (
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
             <Navbar />
 
             <main>
@@ -18,8 +17,6 @@ export const Home = () => {
                 <Contacto />
                 {/*<Footer />*/}
             </main>
-
-
         </div>
     );
-}
+};
